82b726be826f734766cdcf1c21341328bebf3138
Yusuke Ito <novi.mad@gmail.com>
2016-11-28 15:38:14 +0900

    relayout modulemap

--- Package.swift
+++ Package.swift
@@ -1,10 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "CMySQL",
-    pkgConfig: "mariadb",
-    providers: [
-      .Brew("mariadb"),
-      .Apt("libmariadbclient-dev")
-    ]
+    name: "CMySQL"
 )
